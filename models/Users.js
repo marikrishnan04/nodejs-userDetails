f24aace34ce3f79de30796594b7a6ecda0b8d6ca
“marikrishnan04” <krishmarish777@gmail.com>
2023-10-19 06:26:24 -0700

    flie

--- models/Users.js
+++ models/Users.js
@@ -34,7 +34,7 @@ const joi = require('joi');
 // You can add Joi validation for your user data if needed, like this:
 const loginuserSchema = (user) => {
     const schema = joi.object({
-        userName: joi.string().required(),
+        userName: joi.string().min(2).max(30).required(),
         password: joi.string().min(3).required(),
         token: joi.string(),
         email: joi.string().email().required(),
