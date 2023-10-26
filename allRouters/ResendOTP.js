055a3961e7edbe2a25b21ac388a15eef514bf857
“marikrishnan04” <krishmarish777@gmail.com>
2023-10-26 01:35:25 -0700

    flie

--- allRouters/ResendOTP.js
+++ allRouters/ResendOTP.js
@@ -1,26 +1,28 @@
-const ResendOTPRouter = require("express").Router();
-const bodyParser = require("body-parser");//////////
-const bcrypt = require("bcrypt");
-const loginUsers = require("../models/Users");
-const Joi = require("joi");
-const sendEmail = require("../models/sendEmail");
+const express = require("express");
+const ResendOTPRouter = express.Router();
 const otp_Verification = require("../models/OTP_Verification/OTP_Verification");
 const { sendOTPVerification } = require("./sendOTPVerification");
 
-ResendOTPRouter.post('/',async (req,res)=>{
-    try{
-        const {userId,email}=req.body
-        if(!userId || !email){
-            res.send('empty user not send ')
-        }else{
-            await otp_Verification.deleteMany({ userId });
-            sendOTPVerification({_id:userId,email},res);
-            res.send('success')
-        }
-  
-    }catch(error){
-  
-    }
-  })
+ResendOTPRouter.use(express.json()); // Use express.json() for parsing JSON data
 
-  module.exports=ResendOTPRouter;
+ResendOTPRouter.post("/", async (req, res) => {
+  try {
+    const { userId, email } = req.body;
+    if (!userId || !email) {
+      res.status(400).json({ error: "User ID and email are required" });
+    } else {
+      // Delete existing OTP records for the user
+      await otp_Verification.deleteMany({ userId });
+
+      // Send a new OTP
+      sendOTPVerification({ _id: userId, email }, res);
+
+      res.json({ message: "Success: OTP has been resent" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = ResendOTPRouter;
