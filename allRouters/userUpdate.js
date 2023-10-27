dd28eaae1d818b5fa34f3261f22ec3b743c4196d
“marikrishnan04” <krishmarish777@gmail.com>
2023-10-26 22:37:17 -0700

    flie

--- allRouters/userUpdate.js
+++ allRouters/userUpdate.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const loginUsersUpdateRouter = express.Router();
 const Users = require("../models/Users"); // Correct model name
+const Joi = require("joi");
+
 
 loginUsersUpdateRouter.put("/:id", async (req, res) => {
     console.log(req.body);
     try {
+        const schema = Joi.object({
+            Repeat_Password: Joi.string().required(),
+            password: Joi.string().required(),
+          });
+          const { error } = schema.validate(req.body);
+          if (error) return res.status(400).send(error.details[0].message);
         const loginUsers_employees = await Users.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
         if (!loginUsers_employees) {
