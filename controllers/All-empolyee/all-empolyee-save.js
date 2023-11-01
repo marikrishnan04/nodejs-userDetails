8cb37f2da9bcea58c5505276862ab8aab31343f4
“marikrishnan04” <krishmarish777@gmail.com>
2023-11-01 02:41:11 -0700

    flie

--- controllers/All-empolyee/all-empolyee-save.js
+++ controllers/All-empolyee/all-empolyee-save.js
@@ -4,56 +4,36 @@ const allEmpolyeeAddRouter = express.Router();
 const allEmployee_Joi_schema = require("../../models/all-empolyeeSchema/allEmoleeSchema_joi");
 
 allEmpolyeeAddRouter.post("/", async (req, res) => {
-  try {
-    const {
-        First_Name,
-        last_Name,
-        User_Name,
-        Email,
-        password,
-        Confirm_Password,
-        Employee_ID,
-        Mobile_No,
-        Department,
-        Designation,
-        Joining_Date
-    } = req.body;
+    try {
+        const {First_Name,last_Name,User_Name,Email,password,Confirm_Password,Employee_ID, Mobile_No,Department,Designation,Joining_Date } = req.body;
 
-    // Check if the password and Confirm_Password match
-    if (password !== Confirm_Password) {
-        return res.status(400).send("Passwords do not match");
+        const allEmpolyee = new allemployee({
+            First_Name,
+            last_Name,
+            User_Name,
+            Email,
+            password,
+            Confirm_Password,
+            Employee_ID,
+            Mobile_No,
+            Department,
+            Designation,
+            Joining_Date
+        });
+ 
+          const { error } = allEmployee_Joi_schema.validate(req.body);
+
+          if (error) {
+            return res.status(400).send(error.details[0].message);
+          }
+  
+        const savedallEmpolyee = await allEmpolyee.save();
+
+        res.status(201).json({ message: "Success", allEmpolyee: savedallEmpolyee });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal server error" });
     }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const allEmployee = new allemployee({
-        First_Name,
-        last_Name,
-        User_Name,
-        Email,
-        password: hashedPassword, // Store the hashed password
-        Employee_ID,
-        Mobile_No,
-        Department,
-        Designation,
-        Joining_Date
-    });
-
-    const { error } = allEmployee_Joi_schema.validate(req.body);
-
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-
-    const savedEmployee = await allEmployee.save();
-
-    res.status(201).json({ message: "Success", allEmployee: savedEmployee });
-} catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
-}
-
 });
 
 module.exports = allEmpolyeeAddRouter;
