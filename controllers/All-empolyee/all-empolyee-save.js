4cfdc69a5992c0edddc0461879d138e531fbc410
“marikrishnan04” <krishmarish777@gmail.com>
2023-11-01 02:12:24 -0700

    flie

--- controllers/All-empolyee/all-empolyee-save.js
+++ controllers/All-empolyee/all-empolyee-save.js
@@ -1,35 +1,59 @@
 const express = require("express");
-const allemployee = require("../../models/all-empolyeeSchema/allEmpolyeeSchema");
+const allemployee = require("../../models/all-employeeSchema/allEmployeeSchema");
+const bcrypt = require("bcrypt"); // Import the bcrypt library
 const allEmpolyeeAddRouter = express.Router();
-const allEmployee_Joi_schema = require("../../models/all-empolyeeSchema/allEmoleeSchema_joi");
+const allEmployee_Joi_schema = require("../../models/all-employeeSchema/allEmployeeSchema_joi");
 
 allEmpolyeeAddRouter.post("/", async (req, res) => {
     try {
-        const {First_Name,last_Name,User_Name,Email,password,Confirm_Password,Employee_ID, Mobile_No,Department,Designation,Joining_Date } = req.body;
-
-        const allEmpolyee = new allemployee({
+        const {
             First_Name,
-            last_Name,
+            Last_Name, // Corrected variable name to match the schema
             User_Name,
             Email,
             password,
-            Confirm_Password,
+            Confirm_Password, // Corrected variable name to match the schema
+            Employee_ID,
+            Mobile_No,
+            Department,
+            Designation,
+            Joining_Date
+        } = req.body;
+
+        // Check if the password and Confirm_Password match
+        if (password !== Confirm_Password) {
+            return res.status(400).send("Passwords do not match");
+        }
+
+        // Hash the password using bcrypt
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create a new employee instance with the hashed password
+        const allEmployee = new allemployee({
+            First_Name,
+            Last_Name,
+            User_Name,
+            Email,
+            password: hashedPassword, // Store the hashed password
             Employee_ID,
             Mobile_No,
             Department,
             Designation,
             Joining_Date
         });
- 
-          const { error } = allEmployee_Joi_schema.validate(req.body);
 
-          if (error) {
+        // Validate the employee data using a Joi schema (assuming you have defined it correctly)
+        const { error } = allEmployee_Joi_schema.validate(req.body);
+
+        if (error) {
             return res.status(400).send(error.details[0].message);
-          }
-  
-        const savedallEmpolyee = await allEmpolyee.save();
+        }
 
-        res.status(201).json({ message: "Success", allEmpolyee: savedallEmpolyee });
+        // Save the new employee to the database
+        const savedEmployee = await allEmployee.save();
+
+        res.status(201).json({ message: "Employee added successfully", allEmployee: savedEmployee });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Internal server error" });
@@ -37,4 +61,3 @@ allEmpolyeeAddRouter.post("/", async (req, res) => {
 });
 
 module.exports = allEmpolyeeAddRouter;
-
