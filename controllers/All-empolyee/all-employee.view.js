9f87db44512d0a48938e564b67d64e52806f5e69
“marikrishnan04” <krishmarish777@gmail.com>
2023-11-01 02:07:10 -0700

    flie

--- controllers/All-empolyee/all-employee.view.js
+++ controllers/All-empolyee/all-employee.view.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const allemployee = require("../../models/all-empolyeeSchema/allEmpolyeeSchema");
+const allemployee = require("../../models/all-employeeSchema/allEmployeeSchema");
 const allemployeeviewRouter = express.Router();
-
 
 allemployeeviewRouter.get("/", async (req, res) => {
     try {
-        const users = await allemployee.find();
-        res.json(users);
-      } catch (err) {
-        res.status(400).json('Error: ' + err);
-      }
+        const page = parseInt(req.query.page) || 1; // Parse the page query parameter or default to 1
+        const perPage = 10; // Set the number of items per page
+
+        // Sort the allemployee collection by the 'employee' field in ascending order
+        allemployee.find().sort({ employee: 1 })
+            .skip((page - 1) * perPage)
+            .limit(perPage)
+            .then(employees => res.status(200).json(employees))
+            .catch(err => res.status(400).json('error: ' + err));
+    } catch (err) {
+        res.status(500).json('error: ' + err);
+    }
 });
 
 module.exports = allemployeeviewRouter;
