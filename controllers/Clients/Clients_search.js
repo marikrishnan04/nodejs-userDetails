421c771c8b1d7a41c99f9bf9ac638942708344c6
“marikrishnan04” <krishmarish777@gmail.com>
2023-11-01 22:57:46 -0700

    flie

--- controllers/Clients/Clients_search.js
+++ controllers/Clients/Clients_search.js
@@ -2,29 +2,27 @@ const express = require("express");
 const Clients = require("../../models/Clients/Clients_schema");
 const ClientsSearchRouter = express.Router();
 
+
 ClientsSearchRouter.get("/:key", async (req, res) => {
     try {
-        const { key } = req.query; // Use query parameter instead of route parameter
-
-        if (!key) {
-            return res.status(400).json({ message: "Search key is required" });
-        }
-
         const Client = await Clients.find({
-            $or: [
-                { client_name: { $regex: key, $options: "i" } }, // Use case-insensitive regex
-                { client_email: { $regex: key, $options: "i" } },
-                { client_company: { $regex: key, $options: "i" } },
-                { client_mobilenumber: { $regex: key, $options: "i" } },
-                { client_address: { $regex: key, $options: "i" } },
-                { date: { $regex: key, $options: "i" } },
+            "$or":[
+                {client_name:{$regex:req.params.key}},
+                {client_email:{$regex:req.params.key}},
+                {client_company:{$regex:req.params.key}},
+                {client_mobilenumber:{$regex:req.params.key}},
+                {client_address:{$regex:req.params.key}},
+                {date:{$regex:req.params.key}},
             ]
-        });
+        })
 
-        res.status(200).json({ message: "Success", Client });
+        res.status(200).json({ message: "Success",Client});
+        
     } catch (err) {
-        res.status(500).json({ error: err.message });
+        res.status(400).json('error: ' + err);
     }
 });
 
 module.exports = ClientsSearchRouter;
+
+
